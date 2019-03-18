b0d1504cfc40c796a968dcef70000444870ddccb
aishuqing <1158142776@qq.com>
2019-03-18 22:21:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/aishuqing/layout/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.aishuqing.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+       setContentView(R.layout.layout);
+       // setContentView(R.layout.layout2);
+         //setContentView(R.layout.layout3);
+    }
+}
